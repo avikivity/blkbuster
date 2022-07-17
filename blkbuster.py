03ffdf8a83b4bb42f41ad153aa9ffa0348adad52
Avi Kivity <avi@scylladb.com>
2022-07-17 22:15:37 +0300

    blkbuster - create a video from a blktrace report

---
+++ blkbuster.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+
+import sys
+import re
+import collections
+import numpy
+import PIL
+import PIL.Image
+import PIL.ImageDraw
+from moviepy.video.VideoClip import VideoClip, DataVideoClip
+
+# blkparse line with a Q entry (queued) and RWD (read/write/discard) op
+line_re = re.compile(r'^[\d,]+\s+\d+\s+\d+\s+([\d\.]+)\s+\d+\s+Q\s+([RWD])S?M?\s+(\d+) \+ (\d+).*')
+
+io = collections.namedtuple('io', ('offset', 'size', 'direction'))
+frames = [[]] # of array of io
+
+frame_rate = 60
+
+frame_time = 1 / frame_rate
+
+max_offset = 0
+
+width = 3840
+height = 2160
+
+time = 0.0
+next_frame = time + frame_time
+for line in sys.stdin.readlines():
+    m = re.match(line_re, line)
+    if not m:
+        continue
+    time, direction, offset, size = m.groups()
+    time = float(time)
+    offset = int(offset)
+    size = int(size)
+    this_io = io(offset, size, direction)
+    while time >= next_frame:
+        frames.append([])
+        next_frame += frame_time
+    frames[-1].append(this_io)
+    max_offset = max(max_offset, offset + size)
+
+def row_col(offset):
+    frac_row = offset / max_offset * height
+    row = int(frac_row)
+    col = int((frac_row - row) * width)
+    return row, col
+
+direction_color = {
+    'R': 'green',
+    'W': 'blue',
+    'D': 'red',
+}
+
+def make_frame(frame):
+    img = PIL.Image.new(mode='RGB', size=(width, height), color='white')
+    # f = numpy.full(shape=(height, width, 3), fill_value=255, dtype=numpy.uint8)
+    draw = PIL.ImageDraw.Draw(img)
+    for io in frame:
+        r1, c1 = row_col(io.offset)
+        r2, c2 = row_col(io.offset + io.size - 1)
+        draw.line([(c1, r1), (c2, r2)], fill=direction_color[io.direction], width=3)
+    return numpy.asarray(img)
+
+clip = DataVideoClip(frames, make_frame, fps=frame_rate)
+
+clip.write_videofile('blkbuster.avi', codec='libx264', audio=False)
